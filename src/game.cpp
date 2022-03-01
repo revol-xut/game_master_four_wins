e06f36d8d23a7c410acb8d21a9af022c9c24c0a9
revol-xut <revol-xut@protonmail.com>
2022-03-01 18:34:09 +0100

    adding network code

--- src/game.cpp
+++ src/game.cpp
@@ -2,12 +2,31 @@
 
 #include "game.hpp"
 
+#include <algorithm>
 #include <fstream>
 #include <charconv>
 #include <chrono>
 #include <iostream>
 #include <string>
 #include <thread>
+#include <random>
+
+auto random_string(std::size_t length) -> std::string {
+    const std::string CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<> distribution(0, CHARACTERS.size() - 1);
+
+    std::string random_string;
+
+    for (std::size_t i = 0; i < length; ++i)
+    {
+        random_string += CHARACTERS[distribution(generator)];
+    }
+
+    return random_string;
+}
 
 auto Game::get_bot_respone(const std::string& path) noexcept -> std::size_t {
     std::ifstream file_handle;
@@ -32,6 +51,77 @@ void Game::write_bot_response(const std::string& path, const std::string& value)
     std::ofstream file_handle(path, std::ios::trunc);
     file_handle << "<" + value;
     file_handle.close();
+}
+
+Game::Game() noexcept {
+    auto connect = [](sio::event& e){
+        auto m = e.get_message()->get_map();
+
+        std::cout << "connect" << std::endl;
+        std::for_each(m.begin(),
+                m.end(),
+                [](const std::pair<std::string, sio::message::ptr> &p) {
+                    std::cout << "{" << p.first << ": " << "}\n";
+                });
+    };
+
+    auto disconnect = [](sio::event& e){
+        auto m = e.get_message()->get_map();
+
+        std::cout << "disconnect" << std::endl;
+        std::for_each(m.begin(),
+                      m.end(),
+                      [](const std::pair<std::string, sio::message::ptr> &p) {
+                          std::cout << "{" << p.first << ": " << "}\n";
+                      });
+
+    };
+
+    auto setup = [](sio::event& e){
+        auto m = e.get_message()->get_map();
+
+        std::cout << "setup" << std::endl;
+        std::for_each(m.begin(),
+                      m.end(),
+                      [](const std::pair<std::string, sio::message::ptr> &p) {
+                          std::cout << "{" << p.first << ": " << "}\n";
+                      });
+    };
+
+    auto state_update = [](sio::event& e){
+        auto m = e.get_message()->get_map();
+
+        std::cout << "state_update" << std::endl;
+        std::for_each(m.begin(),
+                      m.end(),
+                      [](const std::pair<std::string, sio::message::ptr> &p) {
+                          std::cout << "{" << p.first << ": " << "}\n";
+                      });
+    };
+
+    client_black_.socket()->on("connect", connect);
+    client_white_.socket()->on("connect", connect);
+
+    client_black_.socket()->on("disconnect", disconnect);
+    client_white_.socket()->on("disconnect", disconnect);
+
+    client_black_.socket()->on("setup", setup);
+    client_white_.socket()->on("setup", setup);
+
+    client_black_.socket()->on("state_update", state_update);
+    client_white_.socket()->on("state_update", state_update);
+
+    client_black_.connect(kAddress);
+    client_white_.connect(kAddress);
+    session_id_ = random_string(12);
+
+    sio::message::ptr m = sio::object_message::create();
+    m->get_map()["sessionId"] = sio::string_message::create(session_id_);
+    //m->insert("sessionId", session_id_);
+    sio::message::list li{m};
+
+    client_black_.socket()->emit("setup", li);
+    client_white_.socket()->emit("setup", li);
 }
 
 void Game::mainloop() noexcept {
@@ -83,7 +173,7 @@ void Game::mainloop() noexcept {
 
         file = current_player ? player_1_file : player_2_file;
         write_bot_response(file, std::to_string(column));
-        std::this_thread::sleep_for(1s);
+        std::this_thread::sleep_for(0.25s);
         // now the bot flushes the file
     }
     trunc_file(player_1_file);
@@ -94,3 +184,16 @@ void Game::trunc_file(const std::string &path) noexcept {
     std::ofstream file_handle(path, std::ios::trunc);
     file_handle.close();
 }
+
+void Game::write_to_website(GamePosition player, int column){
+    auto m = sio::int_message::create(column);
+    sio::message::list li{m};
+
+    if(player == GamePosition::Black){
+        client_black_.socket()->emit("placement", m);
+    } else {
+        client_white_.socket()->emit("placement", m);
+    }
+
+}
+
--- src/game.hpp
+++ src/game.hpp
@@ -2,17 +2,25 @@
 #define GAME_HPP
 
 #include "game_field.hpp"
+
+#include <sio_client.h>
 #include <string>
+
+constexpr const char* kAddress = "http://0.0.0.0:8080";
 
 class Game {
 private:
     GameField board_{};
+    sio::client client_black_;
+    sio::client client_white_;
+    std::string session_id_;
 
     static auto get_bot_respone(const std::string& path) noexcept -> std::size_t;
     static void write_bot_response(const std::string& path, const std::string& value) noexcept;
     static void trunc_file(const std::string& path) noexcept;
+    void write_to_website(GamePosition player, int column);
 public:
-    Game() noexcept = default;
+    Game() noexcept;
     ~Game() noexcept = default;
 
     void mainloop() noexcept;
